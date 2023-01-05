c36898564d74d0bab4870736c8c9d43eed113da4
Alfredo Casanova <atcasanova@gmail.com>
2023-01-05 11:18:01 -0300

    mudança para debug

--- bot.sh
+++ bot.sh
@@ -57,7 +57,7 @@ newchurras(){
 }
 
 newplace(){
-    [ "$username" == "atc1235" ] || return 6;
+    [ "$username" == "$ADMIN" ] || return 6;
     (( $# != 3 )) && return 2
     local venue="$1"
     local latitude="$2"
@@ -101,14 +101,14 @@ offset=$(cat offset)
 while true; do 
     for linha in $(curl -s -X POST --data "offset=$offset&limit=1" "$apiurl/getUpdates" | \
     jq '.result[] | "\(.update_id)|\(.message.message_id)|\(.message.chat.id)|\(.message.from.username)|\(.message.date)|\(.message.location.latitude)|\(.message.location.longitude)|\(.message.location.live_period)|\(.message.text)"' -r| tr '\n' ' ' | sed 's/\\n//g' | sed 's/ /_/g' 2>/dev/null); do
-        
+
         # atribuimos os campos filtrados do json à variáveis que usaremos daqui em diante
         IFS='|' read offset messageId chatid username data latitude longitude live_period text <<< "$linha"
-        
+        echo "$offset|$messageId|$chatid|$username|$data|$latitude|$live_period|$text"
         # se a origem da mensagem ($chatid) não for o $CHATID do grupo (cadastrado no arquivo env.sh)
         # ignoramos e vamos para a próxima
         # isso faz o bot ignorar mensagens enviadas no privado
-        (( ${chatid//null/$CHATID} != $CHATID )) && continue
+        (( ${chatid//null/$CHATID} != $CHATID )) && { offset; continue; }
 
         # se a mensagem enviada for uma live location, o campo 'live_period' do JSON vem prenchido
         # então se ele não for nulo, tratamos a mensagem
@@ -141,12 +141,11 @@ while true; do
                     echo "$username:$lugar:$(date +%s)" >> C_${lugar// /_}_${data//\//}
                 }
             else
-                offset
                 envia "Checkin proibido, ou tá longe ou passou da hora. Chora, @$username"
             fi
         
         # se for uma mensagem de localização normal (live_location vazio, mas longitude preenchida)
-        # apaga a mensagem
+        # apaga a mensagem e compensa o offset
         elif [ "$longitude" != "null" ]; then
             curl -s "$apiurl/deleteMessage?chat_id=$CHATID&message_id=$messageId" && offset
         else
