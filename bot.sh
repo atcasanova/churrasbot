a9f04d51429316a73decd30c50f7529b57b44b43
Alfredo Casanova <atcasanova@gmail.com>
2023-02-23 16:42:35 -0300

    fix(variaveis erradas no churrasAtivo)

--- bot.sh
+++ bot.sh
@@ -281,7 +281,7 @@ churrasAtivo(){
     while IFS='|' read p d t pin; do
         horario_minimo=$(( $(date -d "${d:3:2}/${d:0:2}/${d:6:4} $t" +%s) - 1800 ))
         horario_maximo=$(( $(date -d "${d:3:2}/${d:0:2}/${d:6:4} $t" +%s) + 7200 ))
-        (( $now <= $churras_fim && $now >= $churras_inicio )) && {
+        (( $now <= $horario_maximo && $now >= $horario_minimo )) && {
             IFS='|' read lugar data hora pin <<< "$p|$d|$t|$pin"
             echo "$lugar $data $hora $pin ativo"
             return 0
