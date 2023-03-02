0cf6f7c844cd196ce010460ee45cda92be542d31
Alfredo Casanova <atcasanova@gmail.com>
2023-03-02 18:07:42 -0300

    main as function

---
+++ src/main.sh
@@ -0,0 +1,26 @@
+main(){
+    for linha in $(curl -s -X POST --data "offset=$offset&limit=1" "$apiurl/getUpdates" | \
+    jq '.result[] | "\(.update_id)|\(.message.message_id)|\(.message.chat.id)|\(.message.from.username)|\(.message.from.id)|\(.message.date)|\(.message.location.latitude)|\(.message.location.longitude)|\(.message.location.live_period)|\(.message.text)"' -r| tr '\n' ' ' | sed 's/\\n//g' | sed 's/ /_/g' 2>/dev/null); do
+        # atribuimos os campos filtrados do json à variáveis que usaremos daqui em diante
+        IFS='|' read offset messageId chatid username userid data latitude longitude live_period text <<< "$linha"
+        echo "$offset|$messageId|$chatid|$userid|$username|$data|$latitude|$live_period|$text"
+        captureUser "$username"
+        # se a origem da mensagem ($chatid) não for o $CHATID do grupo (cadastrado no arquivo env.sh)
+        # ignoramos e vamos para a próxima
+        # isso faz o bot ignorar mensagens enviadas no privado
+        (( ${chatid//null/$CHATID} != $CHATID )) && { offset; continue; }
+
+        # se a mensagem enviada for uma live location, o campo 'live_period' do JSON vem prenchido
+        # então se ele não for nulo, tratamos a mensagem
+        if [ "$live_period" != "null" ]; then
+            handleLiveLocation
+        # se for uma mensagem de localização normal (live_location vazio, mas longitude preenchida)
+        # apaga a mensagem e compensa o offset
+        elif [ "$longitude" != "null" ]; then
+            echo "Mensagem de localização detectada vindo de @$username. Tentando apagar $messageId em $offset"
+            curl -s "$apiurl/deleteMessage?chat_id=$CHATID&message_id=$messageId" && offset
+        else
+            handleMessage
+        fi
+    done
+}
--- src/notificaUsers.sh
+++ src/notificaUsers.sh
@@ -2,9 +2,9 @@
 # função para notificar com @mention todos os usuários
 # é feito por userId para continuar notificando se o usuário mudar o nick
 notificaUsers(){
-    local id username msg="Atenção! "
-    while read id; do
-        username=$(curl -s "$apiurl/getChatMember?chat_id=$CHATID&user_id=$id" | jq -r '.result.user.username')
+    local username msg="⚠️ ⚠️ Atenção ⚠️ ⚠️ "
+    while read username; do
+        # username=$(curl -s "$apiurl/getChatMember?chat_id=$CHATID&user_id=$id" | jq -r '.result.user.username')
         [ "$username" == "null" -o "@$username" = "$BOTNAME" ] && continue
         msg+="@$username "
     done < members
