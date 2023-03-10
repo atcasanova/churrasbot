a4f888f7614c02079581a443733e802a042ad3e5
Alfredo Casanova <atcasanova@gmail.com>
2023-03-10 18:04:37 -0300

    fix(wat wat)

--- src/timeLimit.sh
+++ src/timeLimit.sh
@@ -1,8 +1,7 @@
 timeLimit(){
-    local d=$1 t=$2 horas_limite now churras_timestamp diff
+    local d="$1" t="$2" horas_limite now churras_timestamp diff
     horas_limite=18
     horas_limite=$(( $horas_limite * 60 * 60 )) # horas em segundos
-    read d t 
     now=$(date +%s)
     churras_timestamp=$(( $(date -d "${d:3:2}/${d:0:2}/${d:6:4} $t" +%s) )) 
     diff=$(( $churras_timestamp - $now ))
