b55987d5b8b6aa972943b1d0872073db9872e192
Alfredo Casanova <atcasanova@gmail.com>
2023-03-11 14:10:52 -0300

    fix(do)

--- src/checkOverlap.sh
+++ src/checkOverlap.sh
@@ -4,7 +4,7 @@ checkOverlap(){
     start1="$(date -d "$data $hora -03 -1 hour" +%s)"
     end1="$(date -d "$data $hora -03 +2 hours" +%s)"
     
-    while IFS='|' read p d t pin
+    while IFS='|' read p d t pin; do
         start2=$( date -d "${d:3:2}/${d:0:2}/${d:6:4} $t -03 -1 hour" +%s )
         end2=$( date -d "${d:3:2}/${d:0:2}/${d:6:4} $t -03 +2 hour" +%s )
         if [[ "$end1" < "$start2" ]] || [[ "$start1" > "$end2" ]]; then
