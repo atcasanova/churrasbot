e9578e37fac8c1d810b20370579f68d94964d8e7
Alfredo Casanova <atcasanova@gmail.com>
2023-03-17 20:26:53 -0300

    refactor

--- src/churrasAtivo.sh
+++ src/churrasAtivo.sh
@@ -3,8 +3,9 @@ churrasAtivo(){
     local p d t id now horario_minimo horario_maximo
     now=$(date +%s)
     while IFS='|' read p d t id; do
-        horario_minimo=$(( $(date -d "${d:3:2}/${d:0:2}/${d:6:4} $t" +%s) - ($ANTES*3600) ))
-        horario_maximo=$(( $(date -d "${d:3:2}/${d:0:2}/${d:6:4} $t" +%s) + ($DEPOIS*3600) ))
+        tmpd=$(date -d "${d:3:2}/${d:0:2}/${d:6:4} $t" +%s)
+        horario_minimo=$(( $tmpd - ($ANTES*3600) ))
+        horario_maximo=$(( $tmpd + ($DEPOIS*3600) ))
         if (( $now <= $horario_maximo && $now >= $horario_minimo )); then
             IFS='|' read lugar data hora pin <<< "$p|$d|$t|$id"
             echo "$lugar $data $hora $pin ativo"
