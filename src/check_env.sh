342767dc474bdc9ca7967b8ccc7e9da4112c8347
Alfredo Casanova <atcasanova@gmail.com>
2023-04-11 23:05:25 -0300

    Update check_env.sh

--- src/check_env.sh
+++ src/check_env.sh
@@ -15,28 +15,32 @@ check_env(){
 	EOF
         error "Arquivo env.sh não existe. Criei um modelo. Edite-o"
     else
-        source env.sh
-
-        # Verifica a existência das dependências necessárias em um loop
-        for dep in curl bc at; do
-            command -v $dep >/dev/null || error "$dep não instalado"
-        done
-
-        # Verifica se o mailx está instalado se o email estiver habilitado
-        mailEnabled && { command -v mailx >/dev/null || error "mailx não instalado"; }
-
-        # Cria o diretório 'reminders' se não existir
-        [ ! -d reminders ] && mkdir reminders
-
-        # Valida as variáveis do ambiente
-        [[ ! "$TOKEN" =~ [0-9]{9}:[a-zA-Z0-9_-]{35} ]] && error "Variável TOKEN inválida"
-        [ -z "$CHATID" ] && error "Variável CHATID vazia"
-        [[ ! "$(declare -p ADMINS)" =~ "declare -a" ]] && error "Variável ADMINS deve ser um array."
-        (( ${#ADMINS[@]} < 1 )) && error "Array ADMINS sem elementos"
-        (( ${#ADMINS[0]} < 1 )) && error "Array ADMINS deve ter ao menos um elemento"
+        local current_timestamp=$(stat -c %Y "env.sh")
+        if [[ ! -v file_timestamps["env.sh"] ]] || (( ${file_timestamps["env.sh"]} != $current_timestamp )); then
+            echo "[UPDT] file $env.sh created or updated"
+            source env.sh
+            file_timestamps["env.sh"]=$current_timestamp
         
-        # Preenche BOTNAME
-        [ -z $BOTNAME ] && BOTNAME="@$(curl -s $apiurl/getMe | jq -r '.result.username')"
+            # Verifica a existência das dependências necessárias em um loop
+            for dep in curl bc at; do
+                command -v $dep >/dev/null || error "$dep não instalado"
+            done
+
+            # Verifica se o mailx está instalado se o email estiver habilitado
+            mailEnabled && { command -v mailx >/dev/null || error "mailx não instalado"; }
+
+            # Cria o diretório 'reminders' se não existir
+            [ ! -d reminders ] && mkdir reminders
+
+            # Valida as variáveis do ambiente
+            [[ ! "$TOKEN" =~ [0-9]{9}:[a-zA-Z0-9_-]{35} ]] && error "Variável TOKEN inválida"
+            [ -z "$CHATID" ] && error "Variável CHATID vazia"
+            [[ ! "$(declare -p ADMINS)" =~ "declare -a" ]] && error "Variável ADMINS deve ser um array."
+            (( ${#ADMINS[@]} < 1 )) && error "Array ADMINS sem elementos"
+            (( ${#ADMINS[0]} < 1 )) && error "Array ADMINS deve ter ao menos um elemento"
+        
+            # Preenche BOTNAME
+            [ -z $BOTNAME ] && BOTNAME="@$(curl -s $apiurl/getMe | jq -r '.result.username')"
+        fi
     fi
-    curl -s $apiurl/getMe >/dev/null
 }
